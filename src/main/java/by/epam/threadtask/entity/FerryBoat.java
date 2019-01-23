56455d74282d99aa98926d885f22b6463fd42a95
Dima <Dimon.Gritsuk@mail.ru>
2019-01-24 02:22:32 +0300

    change creating Singleton

--- src/main/java/by/epam/threadtask/entity/FerryBoat.java
+++ src/main/java/by/epam/threadtask/entity/FerryBoat.java
@@ -3,33 +3,48 @@ package by.epam.threadtask.entity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class FerryBoat {
-    private final static Logger LOGGER=LogManager.getLogger(FerryBoat.class);
-    private static FerryBoat INSTANCE=null;
+    private final static Logger LOGGER = LogManager.getLogger(FerryBoat.class);
+    private static FerryBoat INSTANCE = null;
     private int boatArea;
     private int boatStrong;
+    private static Lock lock = new ReentrantLock();
 
-    private FerryBoat(){};
+    private FerryBoat() {
+    }
 
-    public static FerryBoat getInstance(){
-        if(INSTANCE==null){
-            synchronized (FerryBoat.class){
-                if(INSTANCE==null){
-                    INSTANCE=new FerryBoat();
+    ;
+
+    public static FerryBoat getInstance() {
+        if (INSTANCE == null) {
+            lock.lock();
+            try {
+                if (INSTANCE == null) {
+                    INSTANCE = new FerryBoat();
                 }
+            } finally {
+                lock.unlock();
             }
         }
         return INSTANCE;
     }
-    public void setBoatArea(int boatArea){
-        this.boatArea=boatArea;
+
+    public void setBoatArea(int boatArea) {
+        this.boatArea = boatArea;
     }
 
-    public int getBoatArea(){return boatArea;}
-
-    public void setBoatStrong(int boatStrong){
-        this.boatStrong=boatStrong;
+    public int getBoatArea() {
+        return boatArea;
     }
 
-    public int getBoatStrong(){return boatStrong;}
+    public void setBoatStrong(int boatStrong) {
+        this.boatStrong = boatStrong;
+    }
+
+    public int getBoatStrong() {
+        return boatStrong;
+    }
 }
--- src/main/java/by/epam/threadtask/service/parser/XMLParser.java
+++ src/main/java/by/epam/threadtask/service/parser/XMLParser.java
@@ -1,7 +1,7 @@
 package by.epam.threadtask.service.parser;
 
 public class XMLParser {
-    public void buildCarSet(String dilePath){
-
+    public void buildCarSet(String filePath){
+        
     }
 }
